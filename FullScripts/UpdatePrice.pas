c0b2ea6bbc62e75a30bc0d2b46132f5a232320fd
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-06-20 15:19:20 +0300

    Create UpdatePrice.pas

---
+++ FullScripts/UpdatePrice.pas
@@ -0,0 +1,100 @@
+const
+  CREATE_REPRICE = 'INSERT INTO REPRICES (ID, NAME, CREATORID, APPROVED, NOTE, EFFECTDATE) VALUES (gen_id(GEN_REPRICES, 1), :NAME, :USERID, 0, :NOTE, :DATE) RETURNING ID';
+  GET_LAST_REPRICEID = 'SELECT MAX(ID) as ID FROM REPRICES';
+  FIND_ARTICULID = 'SELECT MAX(ARTICULID) as ARTICULID FROM VIRTARTICULES WHERE AR_ART = :AR_ART and AR_LONGNAME = :AR_LONGNAME';
+  CREATE_REPRICE_ITEM = 'INSERT INTO REPRICES_ITEMS (ID, REPRICEID, ARTICULID, PRICE) VALUES (gen_id(GEN_REPRICES_ITEMS, 1), :REPRICEID, :ARTICULID, :PRICE)';
+
+var
+  S: IomSession;
+  vFileName, currStr: String;
+  entryDict: IcmDictionary;
+  sFile: TStringList;
+  ViewsUIService: IpubViewsUIService;
+  ObjectsUIService: IpubObjectsUIService;
+  RP: IowReprice;
+  RI: IowRepriceItem;
+  visible: Boolean = True;
+
+function getDictionaryFromEntry(str: string): IcmDictionary;
+var
+  dict: IcmDictionary;
+begin
+  dict := CreateDictionary;
+  index := 0;
+  str := str + ';';
+
+  while str <> '' do begin
+    value := Copy(str, 1, Pos(';', str) - 1);
+    DeleteStr(str, 1, Pos(';', str));
+    dict.Add(intToStr(index), value);
+    index := index + 1;
+  end;
+
+  Result := dict;
+end;
+
+
+begin
+  if PromptForFileName(vFileName, 'CSV files (*.csv)|*.csv', '', 'Выберите файл', 'C:\', false) then begin
+    S := CreateObjectSession();
+    sFile := TStringList.Create;
+    sFile.LoadFromFile(vFileName);
+
+    REPRICENAME := 'Переоценка от ' + DateTimeToStr(Now);
+    REPRICENOTE := 'Переоценка, созданная с помощью скрипта, от ' + DateTimeToStr(Now);
+
+    RP := S.NewObject(IowReprice);
+    RP.Name := REPRICENAME;
+    RP.Comment := REPRICENOTE;
+    RP.EffectDate := Now + 1/24;
+
+    for i := 1 to sFile.count - 1 do begin
+      currStr := sFile[i];
+
+      if currStr <> '' then begin
+        entryDict := getDictionaryFromEntry(currStr);
+
+        ART := entryDict['0'];
+        NAME := entryDict['1'];
+        DOP_ART := entryDict['2'];
+        C_TYPE := entryDict['3'];
+        MD_NAME := entryDict['4'];
+        PRICE := entryDict['5'];
+
+        ARTICULID := S.QueryValue(FIND_ARTICULID, MakeDictionary([
+          'AR_ART', ART,
+          'AR_LONGNAME', NAME
+        ]));
+
+        try
+          RI := RP.Items.Add(S.OpenObject(IowArticul, ARTICULID));
+        except
+          visible := false;
+          showMessage('Артикул "' + ART + '" (строка №' + intToStr(i + 1) + ') не найден в программе - переоценка не будет сформирована');
+          break;
+        end;
+
+        try
+          RI.Price := PRICE;
+          RI.Apply;
+        except
+          visible := false;
+          showMessage('На артикуле "' + ART + '" (строка №' + intToStr(i + 1) + ') стоит пустая цена - ' + #13 + 'переоценка не будет сформирована');
+          break;
+        end;
+      end;
+    end;
+
+    if visible then begin
+      ViewsUIService := ServiceProvider.GetService(IpubViewsUIService);
+      ObjectsUIService := ServiceProvider.GetService(IpubObjectsUIService);
+
+      if not ViewsUIService.ActivateView('Reprices') then
+        ViewsUIService.OpenView('Reprices', True)
+      else
+        ViewsUIService.RefreshActiveView;
+
+      ObjectsUIService.EditObject(RP);
+    end;
+  end;
+end;
