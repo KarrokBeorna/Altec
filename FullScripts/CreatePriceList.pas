d06b9e454683300e7b31dd2a9890f00bec98b764
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-06-20 15:21:46 +0300

    Create CreatePriceList.pas

---
+++ FullScripts/CreatePriceList.pas
@@ -0,0 +1,58 @@
+const
+  selectPriceList =
+   'SELECT                                                          ' + #13#10 +
+   '  PT_NAME,                                                      ' + #13#10 +
+   '  RIT.it_name as TYPE_NAME,                                     ' + #13#10 +
+   '  RFP.FP_NAME as PROFIL_NAME,                                   ' + #13#10 +
+   '  RFF.FF_NAME as FURN_NAME,                                     ' + #13#10 +
+   '  CO.CP_NAME as CO_NAME,                                        ' + #13#10 +
+   '  CI.CP_NAME as CI_NAME,                                        ' + #13#10 +
+   '  PV_VALUE as PRICE,                                            ' + #13#10 +
+   '  PH_VALUE as HEIGHT,                                           ' + #13#10 +
+   '  PW_VALUE as WIDTH                                             ' + #13#10 +
+   'FROM p_templates PT                                             ' + #13#10 +
+   '  LEFT JOIN p_values PV ON PV.PT_ID = PT.PT_ID                  ' + #13#10 +
+   '  LEFT JOIN colorspart CI ON CI.colorspartid = PT.INCOLORID     ' + #13#10 +
+   '  LEFT JOIN colorspart CO ON CO.colorspartid = PT.OUTCOLORID    ' + #13#10 +
+   '  LEFT JOIN r_itemstype RIT ON RIT.itemstypeid = PT.ITEMSTYPEID ' + #13#10 +
+   '  LEFT JOIN r_firmprofil RFP ON RFP.firmid = PT.FIRMID          ' + #13#10 +
+   '  LEFT JOIN r_firmfurn RFF ON RFF.firmid = PT.FIRMFURNID        ' + #13#10 +
+   'WHERE ';
+
+var
+  vFileName, PT_IDs : string;
+  session: IomSession;
+  priceList, SQLD: variant;
+  i: integer;
+  buf: TStringList;
+
+procedure createTable(initialRow, initialColumn: Integer);
+begin
+  for i := 0 to priceList.Count - 1 do begin
+    buf.Add(priceList.items[i].value['PT_NAME'] + ' ' + varToStr(priceList.items[i].value['WIDTH']) + 'x' + varToStr(priceList.items[i].value['HEIGHT']) + ';' + varToStr(priceList.items[i].value['PRICE']));
+  end;
+end;
+
+begin
+  if PromptForFileName(vFileName, 'CSV files (*.csv)|*.csv', '', 'Место сохранения', 'C:\', true) then begin
+// Если что это не я дурак, а модуль Дельфи, работающий с БД:
+// он не даёт прописать в запросе *** WHERE PT.PT_ID in (:PT_IDs) ***,
+// чтобы в запрос потом добавлять только строку из IDs
+    PT_IDs := 'PT.PT_ID in (';
+    for i := 0 to SelectedRecords.Count - 2 do begin
+      PT_IDs := PT_IDs + varToStr(SelectedRecords.items[i].Value['PT_ID']) + ',';
+    end;
+    PT_IDs := PT_IDs + varToStr(SelectedRecords.items[SelectedRecords.Count - 1].Value['PT_ID']) + ')';
+
+    session := CreateObjectSession();
+    SQLD := MakeDictionary(['PT_IDs', PT_IDs]);
+    priceList := session.QueryRecordList(selectPriceList + PT_IDs, empty);
+
+    buf := TStringList.Create;
+    createTable(1, 1);
+    buf.SaveToFile(vFileName + '.csv');
+    buf.Free;
+
+    session.Commit;
+  end;
+end;
