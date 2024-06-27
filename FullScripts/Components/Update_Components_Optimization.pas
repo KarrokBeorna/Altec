015082dd3e22eb428fab8395463e4fad9644bddb
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-06-27 11:41:43 +0300

    Create Update_Components_Optimization.pas

---
+++ FullScripts/Components/Update_Components_Optimization.pas
@@ -0,0 +1,44 @@
+const
+  UPDATE_ARTICUL = 'UPDATE VIRTARTICULES V'                                + #13#10 +
+                   'SET V.OG_ID = :OG_ID, V.BEAMSIZEID = :BEAMSIZEID'      + #13#10 +
+                   'WHERE V.ARTICULID = :SELECTID AND V.DOPMEASUREID IN (' + #13#10 +
+                   '  SELECT'                                              + #13#10 +
+                   '    DOPMEASUREID'                                      + #13#10 +
+                   '  FROM DOPMEASURE D'                                   + #13#10 +
+                   '    JOIN MEASURE M ON D.MEASUREID = M.MEASUREID'       + #13#10 +
+                   '  WHERE M.MEASUREID = 2'                               + #13#10 +
+                   ')';
+
+var
+  S: IomSession;
+  SQLparams: IcmDictionary;
+  ObjectsUIService: IpubObjectsUIService;
+  OG: IowOptimizationGroup;
+  MU: IowMeasureUnit;
+
+procedure btnOkClicked(Sender: TOBject);
+begin
+
+end;
+
+begin
+  S := CreateObjectSession;
+  SQLparams := CreateDictionary;
+  ObjectsUIService := ServiceProvider.GetService(IpubObjectsUIService);
+  OG := ObjectsUIService.SelectObject('IowOptimizationGroup');
+  MU := ObjectsUIService.SelectObject('IowMeasureUnit');
+
+  try
+    for i := 0 to SelectedRecords.Count - 1 do begin
+      SQLparams.Clear;
+      SQLparams.Add('SELECTID', SelectedRecords.Items[i]['ARTICULID']);
+      SQLparams.Add('OG_ID', OG.key);
+      SQLparams.Add('BEAMSIZEID', MU.key);
+      S.ExecSQL(UPDATE_ARTICUL, SQLparams);
+    end;
+    //showmessage('Обновлено записей: ' + IntToStr(SelectedRecords.Count));
+    S.Commit;
+  except
+    S.Rollback;
+  end;
+end;
