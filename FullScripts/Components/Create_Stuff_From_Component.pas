39b976bb54451fd36ae72a8db36563c127febcd3
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-07-09 11:43:10 +0300

    Update Create_Stuff_From_Component.pas

--- FullScripts/Components/Create_Stuff_From_Component.pas
+++ FullScripts/Components/Create_Stuff_From_Component.pas
@@ -3,17 +3,30 @@ var
   KitID : integer;
   ObjectsUIService: IpubObjectsUIService;
   Stuff: IowStuff;
+  LS: BOOLEAN;
 
 begin
-    S := CreateObjectSession();
-    Stuff := S.NewObject(IowStuff);
+  S := CreateObjectSession();
+  Stuff := S.NewObject(IowStuff);
 
-    KitID := S.QueryValue('SELECT MIN(ID) FROM KITTYPES WHERE DELETED = 0', Empty);
+  KitID := S.QueryValue('SELECT MIN(ID) FROM KITTYPES WHERE DELETED = 0', Empty);
 
-    Stuff.Articul := S.OpenObject(IowArticul, Instance.Key);
+  Stuff.Articul := S.OpenObject(IowArticul, Instance.Key);
+
+  if VarIsClear(Instance.Price) or VarIsNull(Instance.Price) then begin
+    Stuff.Price := 0;
+  end else if VarIsClear(Instance.RecalcGroup) or VarIsNull(Instance.RecalcGroup) then begin
+    Stuff.Price := Instance.Price;
+  end else begin
     Stuff.Price := Instance.Price * Instance.RecalcGroup.Factor;
-    Stuff.KitType := S.OpenObject(IowKitType, KitID);
+  end;
 
+  try
+    Stuff.KitType := S.OpenObject(IowKitType, KitID);
     ObjectsUIService := ServiceProvider.GetService(IpubObjectsUIService);
-    ObjectsUIService.EditObject(Stuff);
+    LS := ObjectsUIService.EditObject(Stuff);
+  except
+    showmessage('Занесите какой-либо тип комплектации в разделе "Справочники - Типы комплектации"');
+    Logoff;
+  end;
 end;
