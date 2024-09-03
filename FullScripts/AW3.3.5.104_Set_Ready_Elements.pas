a55b255faac7d958950953140ef392474d3c8304
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-09-03 13:32:48 +0300

    Update AW3.3.5.104_Set_Ready_Elements.pas

--- FullScripts/AW3.3.5.104_Set_Ready_Elements.pas
+++ FullScripts/AW3.3.5.104_Set_Ready_Elements.pas
@@ -4,7 +4,7 @@ const
   SET_CUTTED1_2 = 'UPDATE BEAMS_CUTTING_TASKS SET CLOSED = 1, RESERVED = 0, EXECUTED = 1, USEDAMOUNT = 0, USEDCOST = 0, TECHWASTEAMOUNT = 0, OPTWASTEAMOUNT = 0, WASTECOST = 0   ' + #13#10 +
                   'WHERE FACTORYTASKID = :ID';
   SET_CUTTED2_1 = 'UPDATE FACTORY_UNITS_2D_DETAILS F2 SET CUTTED = 1   ' + #13#10 +
-                'WHERE F2.FACTORYUNITID = :ID';
+                  'WHERE F2.FACTORYUNITID = :ID';
   SET_CUTTED2_2 = 'UPDATE SHEETS_CUTTING_TASKS SET CLOSED = 1, RESERVED = 0, EXECUTED = 1, USEDAMOUNT = 0, USEDCOST = 0, TECHWASTEAMOUNT = 0, OPTWASTEAMOUNT = 0, WASTECOST = 0   ' + #13#10 +
                   'WHERE FACTORYTASKID = :ID';
 
@@ -38,27 +38,40 @@ const
                     '  STAGE = 3                     ' + #13#10 +
                     'WHERE UNITID = :ID';
 
-  DELETE_STAGE_QUEUE = 'DELETE FROM FACTORY_UNITS_QUEUE WHERE STAGE = 3 AND UNITID IN (SELECT ID FROM FACTORY_UNITS WHERE PARENTUNITID = :ID)';
-
-  SET_READY_SHIPPING = 'UPDATE SHIPPING_UNITS SET   ' + #13#10 +
-                       '  READY = 1                 ' + #13#10 +
-                       'WHERE ID = (SELECT SHIPPINGUNITID FROM FACTORY_UNITS WHERE ID = :ID)';
+  DELETE_STAGE_QUEUE = 'DELETE FROM FACTORY_UNITS_QUEUE WHERE STAGE = 3 AND (UNITID IN (SELECT ID FROM FACTORY_UNITS WHERE PARENTUNITID = :ID) OR UNITID = :ID)';
 
   SET_READY_ITEMS = 'UPDATE FACTORY_TASKS_ITEMS SET ' + #13#10 +
                     '  POSTED = 1,                  ' + #13#10 +
                     '  POSTDATE = :PDATE            ' + #13#10 +
                     'WHERE ID = :ID';
 
+  SET_READY_SHIPPING = 'UPDATE SHIPPING_UNITS SET                              ' + #13#10 +
+                       '  READY = 1,                                           ' + #13#10 +
+                       '  ACCEPTED = 1,                                        ' + #13#10 +
+                       '  ACCEPTDATE = :DATE,                                  ' + #13#10 +
+                       '  ACCEPTORID = :ACCEPTORID,                            ' + #13#10 +
+                       '  LOCATION = ''Принят скриптом готовности элементов''  ' + #13#10 +
+                       'WHERE ID = (SELECT SHIPPINGUNITID FROM FACTORY_UNITS WHERE ID = :ID)';
+
+  CHECK_POSTED_ITEMS = 'SELECT MIN(POSTED) FROM FACTORY_TASKS_ITEMS WHERE FACTORYTASKID = :ID';
+
   SET_READY_TASK = 'UPDATE FACTORY_TASKS SET  ' + #13#10 +
                    '  COMPLETED = 1,          ' + #13#10 +
                    '  COMPLETEDATE = :CDATE   ' + #13#10 +
                    'WHERE ID = :ID';
 
+  CHECK_ACCEPTED_SHIPPING = 'SELECT MIN(ACCEPTED) FROM SHIPPING_UNITS WHERE SHIPPINGORDERID = (SELECT FIRST 1 ID FROM SHIPPING_ORDERS WHERE ORDERID = :ID)';
+
+  SET_ACCEPT_SHIPPING = 'UPDATE SHIPPING_ORDERS SET  ' + #13#10 +
+                        '  ACCEPTED = 1              ' + #13#10 +
+                        'WHERE ORDERID = :ID';
+
+
 var
   task: IawFactoryTask;
   facUnit: IawFactoryUnit;
   S: IomSession;
-  checkParts, checkOptimized, checkReserved: Boolean;
+  checkParts, checkOptimized, checkReserved, checkShipping, checkPostedItems: Boolean;
 
 begin
   S := CreateObjectSession;
@@ -78,14 +91,29 @@ begin
           for j := 0 to task.items.count - 1 do begin
             facUnit := task.items.items[j].FactoryUnit;
 
-            S.ExecSQL(SET_CUTTED1_1, MakeDictionary(['ID', facUnit.key]));
-            S.ExecSQL(SET_CUTTED2_1, MakeDictionary(['ID', facUnit.key]));
-            S.ExecSQL(SET_READY_ELEMENTS, MakeDictionary(['PRDATE', Now, 'USERID', CurrentUser.key, 'ID', facUnit.key]));
-            S.ExecSQL(SET_PARTS_PRODUCED, MakeDictionary(['ID', facUnit.key]));
-            S.ExecSQL(SET_READY_ITEMS, MakeDictionary(['PDATE', Now, 'ID', task.items.items[j].key]));
-            S.ExecSQL(SET_STAGE_QUEUE, MakeDictionary(['ID', facUnit.key]));
-            S.ExecSQL(DELETE_STAGE_QUEUE, MakeDictionary(['ID', facUnit.key]));
-            S.ExecSQL(SET_READY_SHIPPING, MakeDictionary(['ID', facUnit.key]));
+            if varIsNull(facUnit.ProductionDate) then begin
+              S.ExecSQL(SET_CUTTED1_1, MakeDictionary(['ID', facUnit.key]));
+              S.ExecSQL(SET_CUTTED2_1, MakeDictionary(['ID', facUnit.key]));
+              S.ExecSQL(SET_READY_ELEMENTS, MakeDictionary(['PRDATE', Now, 'USERID', CurrentUser.key, 'ID', facUnit.key]));
+              S.ExecSQL(SET_PARTS_PRODUCED, MakeDictionary(['ID', facUnit.key]));
+              S.ExecSQL(SET_READY_ITEMS, MakeDictionary(['PDATE', Now, 'ID', task.items.items[j].key]));
+              S.ExecSQL(SET_STAGE_QUEUE, MakeDictionary(['ID', facUnit.key]));
+              S.ExecSQL(DELETE_STAGE_QUEUE, MakeDictionary(['ID', facUnit.key]));
+              S.ExecSQL(SET_READY_SHIPPING, MakeDictionary(['ID', facUnit.key,
+                                                            'DATE', Now,
+                                                            'ACCEPTORID', CurrentUser.key]));
+              S.Commit;
+            end;
+
+            checkShipping := S.QueryValue(CHECK_ACCEPTED_SHIPPING, MakeDictionary(['ID', facUnit.OrderUnit.OrderItem.Order.Key]));
+            if checkShipping then begin
+              S.ExecSQL(SET_ACCEPT_SHIPPING, MakeDictionary(['ID', facUnit.OrderUnit.OrderItem.Order.Key]));
+              S.Commit;
+            end;
+          end;
+
+          checkPostedItems := S.QueryValue(CHECK_POSTED_ITEMS, MakeDictionary(['ID', task.key]));
+          if checkPostedItems then begin
             S.ExecSQL(SET_READY_TASK, MakeDictionary(['CDATE', Now, 'ID', SelectedRecords[i].Value['ID']]));
             S.Commit;
           end;
