215e81743b85442645a50da92dc53d19a67f4ee3
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-09-03 13:32:03 +0300

    Update AW3.1.4.27_Send_Email.pas

--- FullScripts/AW3.1.4.27_Send_Email.pas
+++ FullScripts/AW3.1.4.27_Send_Email.pas
@@ -1,9 +1,11 @@
 const
   SELECT_LAST_STATE = 'SELECT OS.CODE FROM ORDERSTATES OS JOIN ORDERS O ON O.ORDERSTATEID = OS.ORDERSTATEID WHERE ORDERID = :ID';
+  SELECT_ORDNO = 'SELECT ORDERNO FROM ORDERS WHERE ORDERID = :ID';
 
 var
   S: IomSession;
-  stateCode: String;
+  //stateCode: String;
+  ORDNO: String;
   MS: IpubMailer;
   M: IpubEmail;
 
@@ -21,21 +23,22 @@ begin
   if MS.SendEmail(M, False) then
     showmessage('Письмо отправлено по адресу ' + mail)
   else
-    showmessage('Не удалось отправить письмо по адресу ' + mail);
-}
+    showmessage('Не удалось отправить письмо по адресу ' + mail + '. ' + MS.LastError);
+ }
   M := Empty;
   MS := Empty;
 end;
 
 begin
   S := CreateObjectSession('');
-  stateCode := S.QueryValue(SELECT_LAST_STATE, MakeDictionary(['ID', Document.Key]));
+  //stateCode := S.QueryValue(SELECT_LAST_STATE, MakeDictionary(['ID', OrderID]));
+  ORDNO := S.QueryValue(SELECT_ORDNO, MakeDictionary(['ID', OrderID]));
 
-  if (stateCode <> 'dorabotka') and (Document.State.Code = 'dorabotka') then
-    sendMail(Document.Title, 'УКАЖИТЕ_ЗДЕСЬ_АДРЕС_КЛИЕНТА');
+  if (OldStateCode <> 'dorabotka') and (StateCode = 'dorabotka') then
+    sendMail(ORDNO, 'УКАЖИТЕ_ЗДЕСЬ_ПОЧТУ_СОТРУДНИКА');
 {
-    showmessage('Значит, надо отправлять письмо: ' + stateCode + ' - ' + Document.State.Code)
+    showmessage('Значит, надо отправлять письмо: ' + OldStateCode + ' - ' + StateCode)
   else
-    showmessage('Иначе отправлять письмо не нужно: ' + stateCode + ' - ' + Document.State.Code);
+    showmessage('Иначе отправлять письмо не нужно: ' + OldStateCode + ' - ' + StateCode);
 }
 end.
