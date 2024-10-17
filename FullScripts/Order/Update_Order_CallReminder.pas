c89c587e7793819b82c914247ab4116e82735df5
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-10-17 18:20:13 +0300

    Update Update_Order_CallReminder.pas

--- FullScripts/Order/Update_Order_CallReminder.pas
+++ FullScripts/Order/Update_Order_CallReminder.pas
@@ -2,11 +2,13 @@ var
   Form1: TForm;
   btnOk: TButton;
   Session: IomSession;
-  SQLQ: string;
-  SQLparams: IcmDictionary;
   date: TDateTimePicker;
   time: TComboBox;
   timeStrings: TStringList;
+  cbReady: TCheckBox;
+  PanelComment: TGroupBox;
+  comment: TRichEdit;
+  commentText: String;
 
 // См. Instance.StatusList - статусы текущего заказа
 
@@ -18,6 +20,8 @@ begin
       for i := 0 to Instance.StatusList.Count - 1 do begin
         if Instance.StatusList.Items[i].OrderStatus.Key = 7 then begin
           Instance.StatusList.Items[i].PlanDate := StrToDateTime(Copy(dateTimeToStr(date.time), 0, 11) + time.text + ':00');
+          Instance.StatusList.Items[i].Done := cbReady.checked;
+          Instance.StatusList.Items[i].Comment := comment.lines.text;
           Instance.StatusList.Items[i].apply;
         end;
       end;
@@ -25,7 +29,22 @@ begin
    end;
 end;
 
+procedure cbReadyClicked(Sender: TOBject);
 begin
+  if cbReady.checked then begin
+    btnOk.caption := 'Звонок выполнен';
+  end else begin
+    btnOk.caption := 'Установить напоминание';
+  end;
+end;
+
+begin
+
+for i := 0 to Instance.StatusList.Count - 1 do begin
+  if Instance.StatusList.Items[i].OrderStatus.Key = 7 then begin
+    commentText := Instance.StatusList.Items[i].Comment;
+  end;
+end;
 
 timeStrings := TStringList.Create();
 
@@ -37,7 +56,7 @@ end;
 Form1:=TForm.Create(Application);
  with Form1 do begin
    Width := 235;
-   Height := 120;
+   Height := 205;
    caption := 'Напоминание о звонке';
    position := poScreenCenter;
    BorderIcons := biSystemMenu;
@@ -61,10 +80,40 @@ time := TComboBox.Create(Application);
    items := timeStrings;
  end;
 
+cbReady := TCheckBox.Create(Application);
+ with cbReady do begin
+   Left := 10;
+   Top := 35;
+   Caption := 'Выполнено';
+   Parent := Form1;
+   onClick := @cbReadyClicked;
+ end;
+
+PanelComment := TGroupBox.Create(Application);
+ with PanelComment do begin
+   Width := 210;
+   Height := 70;
+   Top := 55;
+   Left := 5;
+   caption := 'Примечание';
+   Parent := Form1;
+ end;
+
+comment := TRichEdit.Create(Application);
+ with comment do begin
+   Left := 5;
+   Top := 15;
+   Width := 200;
+   height := PanelComment.height - 20;
+   Parent := PanelComment;
+ end;
+
+comment.lines.text := commentText;
+
 btnOk := TButton.Create(Application);
  with btnOk do begin
    Left := 10;
-   Top := 45;
+   Top := 130;
    Width := 200;
    Height := 30;
    Caption := 'Установить напоминание';
