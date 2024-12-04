37cf866aca66e32a4c34d45d96383025ae77a6c1
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-12-04 17:42:27 +0300

    Update Update_Components_Measure.pas

--- FullScripts/Components/Update_Components_Measure.pas
+++ FullScripts/Components/Update_Components_Measure.pas
@@ -1,26 +1,31 @@
+{
+ * Для всех выделенных записей в представлении "Компоненты" можно поменять
+ * единицу измерения вне зависимости от того, чем он был раньше и куда он был
+ * добавлен.
+ *
+ * Однако помните, что формулы "Состава" и "Наборов" могут поломаться.
+ * Возможно, что-то сломается и в "Ценах на покрытие", если вы переводите
+ * компонент с единицы измерения "Длина", на какую-нибудь другую
+}
+
 const
   UPDATE_ARTICULES = 'UPDATE VIRTARTICULES V SET V.DOPMEASUREID = :DOPMEASUREID WHERE V.ARTICULID = :SELECTID';
 
 var
   ObjectsUIService: IpubObjectsUIService;
   MU: IowMeasureUnit;
-  SQLparams: IcmDictionary;
   S: IomSession;
 
 begin
   S := CreateObjectSession;
-  SQLparams := CreateDictionary;
   ObjectsUIService := ServiceProvider.GetService(IpubObjectsUIService);
   MU := ObjectsUIService.SelectObject('IowMeasureUnit');
 
   try
     for i := 0 to SelectedRecords.Count - 1 do begin
-      SQLparams.Clear;
-      SQLparams.Add('SELECTID', SelectedRecords.Items[i]['ARTICULID']);
-      SQLparams.Add('DOPMEASUREID', MU.key);
-      S.ExecSQL(UPDATE_ARTICULES, SQLparams);
+      S.ExecSQL(UPDATE_ARTICULES, MakeDictionary(['SELECTID', SelectedRecords.Items[i]['ARTICULID'],
+                                                  'DOPMEASUREID', MU.key]));
     end;
-    //showmessage('Обновлено записей: ' + IntToStr(SelectedRecords.Count));
     S.Commit;
   except
     S.Rollback;
