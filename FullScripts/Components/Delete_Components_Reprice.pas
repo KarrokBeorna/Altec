44bab27b0d90533218344443293a638ec8ffc2e2
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-12-04 18:04:02 +0300

    Update Delete_Components_Reprice.pas

--- FullScripts/Components/Delete_Components_Reprice.pas
+++ FullScripts/Components/Delete_Components_Reprice.pas
@@ -1,3 +1,7 @@
+{
+ * Для всех выделенных записей в представлении "Компоненты" удаляет группу переоценки.
+}
+
 const
   UPDATE_ARTICULES = 'UPDATE VIRTARTICULES V SET V.WGRPRICEID = :WGRPRICEID WHERE V.ARTICULID = :SELECTID';
 
@@ -7,16 +11,12 @@ var
 
 begin
   S := CreateObjectSession;
-  SQLparams := CreateDictionary;
 
   try
     for i := 0 to SelectedRecords.Count - 1 do begin
-      SQLparams.Clear;
-      SQLparams.Add('SELECTID', SelectedRecords.Items[i]['ARTICULID']);
-      SQLparams.Add('WGRPRICEID', Null);
-      S.ExecSQL(UPDATE_ARTICULES, SQLparams);
+      S.ExecSQL(UPDATE_ARTICULES, MakeDictionary(['SELECTID', SelectedRecords.Items[i]['ARTICULID'],
+                                                  'WGRPRICEID', Null]));
     end;
-    //showmessage('Обновлено записей: ' + IntToStr(SelectedRecords.Count));
     S.Commit;
   except
     S.Rollback;
