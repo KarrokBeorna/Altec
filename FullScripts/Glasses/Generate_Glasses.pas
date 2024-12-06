a39feb3d419ad1a3825278e2cb479c4e22cae9e7
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2024-12-06 16:45:20 +0300

    Update and rename OW10_Import_Glasses.pas to Generate_Glasses.pas

--- FullScripts/Glasses/Generate_Glasses.pas
+++ FullScripts/Glasses/Generate_Glasses.pas
@@ -1,3 +1,17 @@
+{
+  * Скрипт сгенерирует все доступные стекопакеты с дистанционными рамками Distance1K и Distance2K
+  * С плёнками Plenka, которые ставятся лишь на первое стекло
+  * Со стёклами первыми уличными или центральными Glasses1
+  * Со стёклами домашними Glasses2
+  * С начальными ценами стёкол Price4 (просто стекло 4мм)
+  *                            Price6 (просто стекло 6мм)
+  *                            Price2K4 (однокамерный стеклопакет со стёклами 4мм)
+  *                            Price3K4 (двухкамерный стеклопакет со стёклами 4мм)
+  * С наценками за покрытие стёкол GPrices
+  * С наценками за дистанционные рамки Dist1Prices и Dist2Prices
+  * С ценами плёнок PlenkaPrices
+}
+
 var
   S: IomSession;
   Price: IowGlassPacketPrice;
@@ -36,6 +50,11 @@ begin
   Dist2Prices := [0, 30, 15, 90];
   PlenkaPrices := [0, 1800, 2000, 2450];
 
+  Price4 := 675;
+  Price6 := 1000;
+  Price2K4 := 1593;
+  Price3K4 := 2079;
+
   index := 1;
 
   // Стекло 4мм
@@ -43,7 +62,7 @@ begin
     for b := 0 to length(Plenka) - 1 do begin
       Formula := Glasses1[a] + Plenka[b];
       index := index + 1;
-      createGlassPacket(675, 0, 0, PlenkaPrices[b], GPrices[a], 0, 0, index, Formula);
+      createGlassPacket(Price4, 0, 0, PlenkaPrices[b], GPrices[a], 0, 0, index, Formula);
     end;
   end;
 
@@ -54,10 +73,10 @@ begin
     for b := 0 to length(Plenka) - 1 do begin
       Formula := Glasses3[a] + Plenka[b];
       index := index + 1;
-      createGlassPacket(1000, 0, 0, PlenkaPrices[b], GPrices[a], 0, 0, index, Formula);
+      createGlassPacket(Price6, 0, 0, PlenkaPrices[b], GPrices[a], 0, 0, index, Formula);
     end;
   end;
-{
+
   index := 0;
 
   // Однокамерные
@@ -67,7 +86,7 @@ begin
         for d := 0 to length(Glasses2) - 1 do begin
           Formula := Glasses1[a] + Plenka[b] + '\' + Distance1K[c] + '\' + Glasses2[d];
           index := index + 1;
-          createGlassPacket(1593, Dist1Prices[c], 0, PlenkaPrices[b], GPrices[a], GPrices[d], 0, index, Formula);
+          createGlassPacket(Price2K4, Dist1Prices[c], 0, PlenkaPrices[b], GPrices[a], GPrices[d], 0, index, Formula);
         end;
       end;
     end;
@@ -84,13 +103,13 @@ begin
             for f := 0 to length(Glasses2) - 1 do begin
               Formula := Glasses1[a] + Plenka[b] + '\' + Distance2K[c] + '\' + Glasses1[d] + '\' + Distance2K[e] + '\' + Glasses2[f];
               index := index + 1;
-              createGlassPacket(2079, Dist2Prices[c], Dist2Prices[e], PlenkaPrices[b], GPrices[a], GPrices[d], GPrices[f], index, Formula);
+              createGlassPacket(Price3K4, Dist2Prices[c], Dist2Prices[e], PlenkaPrices[b], GPrices[a], GPrices[d], GPrices[f], index, Formula);
             end;
           end;
         end;
       end;
     end;
   end;
-}
+
   S.Commit();
 end;
