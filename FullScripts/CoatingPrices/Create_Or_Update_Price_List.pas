6c4f7bf8c6b03a5a8acb8fba5174a4d79d055857
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2025-03-10 14:10:29 +0300

    Update Create_Or_Update_Price_List.pas

--- FullScripts/CoatingPrices/Create_Or_Update_Price_List.pas
+++ FullScripts/CoatingPrices/Create_Or_Update_Price_List.pas
@@ -28,7 +28,7 @@ var
   BaseValue: IowCoatingPriceBaseValue;
   Palette: IowCoatingPalette;
   sFile: TStringList;
-  entryDict: IcmDictionary;
+  entryDict, articulDict: IcmDictionary;
   ArtO: IowArticul;
   visible: Boolean = True;
   PLID, count: Integer;
@@ -59,6 +59,7 @@ begin
   if PromptForFileName(vFileName, 'CSV files (*.csv)|*.csv', '', 'Выберите файл', 'C:\', false) then begin
     sFile := TStringList.Create;
     ObjectsUIService := ServiceProvider.GetService(IpubObjectsUIService);
+    articulDict := CreateDictionary;
 
     RealFileName := ExtractFileName(vFileName);
     PLID := S.QueryValue(FIND_PRICELISTID, MakeDictionary(['NAME', copy(RealFileName, 0, length(RealFileName) - 4)]));
@@ -85,36 +86,57 @@ begin
         OUTPRICE := entryDict['8'];
         INPRICE := entryDict['9'];
         DOUBLEPRICE := entryDict['10'];
+
         ARTICULIDs := S.QueryRecordList(FIND_ARTICULID, MakeDictionary(['AR_ART', ART]));
 
         for z := 0 to ARTICULIDs.Count - 1 do begin
-          ARTICULID := ARTICULIDs.Items[z].value['ARTICULID'];
-          ArtO := S.OpenObject(IowArticul, ARTICULID);
+          ARTICULID := VarToStr(ARTICULIDs.Items[z].value['ARTICULID']);
 
-          if PLID <> 0 then begin
-            count := PriceList.BaseValues.Count - 1;
-            j := 0;
-            while j <= count do begin
-              if ArtO.Key = PriceList.BaseValues[j].Articul.Key then begin
-                PriceList.BaseValues.Delete(PriceList.BaseValues[j]);
-                count := count - 1;
-              end else begin
-                j := j + 1;
-              end;
+          if not articulDict.Exists(ARTICULID) then begin
+            articulDict.add(ARTICULID, MakeDictionary(['ART', ART, 'INPRICE', INPRICE, 'OUTPRICE', OUTPRICE, 'DOUBLEPRICE', DOUBLEPRICE]));
+          end else begin
+            if StrToFloat(articulDict[ARTICULID].Value['INPRICE']) < StrToFloat(INPRICE) then begin
+              t := articulDict[ARTICULID];
+              t.Value['INPRICE'] := INPRICE;
+            end;
+            if StrToFloat(articulDict[ARTICULID].Value['OUTPRICE']) < StrToFloat(OUTPRICE) then begin
+              t := articulDict[ARTICULID];
+              t.Value['OUTPRICE'] := OUTPRICE;
+            end;
+            if StrToFloat(articulDict[ARTICULID].Value['DOUBLEPRICE']) < StrToFloat(DOUBLEPRICE) then begin
+              t := articulDict[ARTICULID];
+              t.Value['DOUBLEPRICE'] := DOUBLEPRICE;
             end;
           end;
+        end;
+      end;
+    end;
 
-          try
-            BaseValue := PriceList.BaseValues.Add(ArtO);
-            BaseValue.InnerPrice := INPRICE;
-            BaseValue.OuterPrice := OUTPRICE;
-            BaseValue.BothPrice := DOUBLEPRICE;
-            BaseValue.Apply;
-          except
-            visible := false;
-            RaiseException('Артикул "' + ART + '" (строка №' + intToStr(i + 1) + ') ошибка во время установки цен - прайс-лист не может быть создан/обновлён');
+    for i := 0 to articulDict.Count - 1 do begin
+      ArtO := S.OpenObject(IowArticul, StrToInt(articulDict.Name[i]));
+
+      if PLID <> 0 then begin
+        count := PriceList.BaseValues.Count - 1;
+        j := 0;
+        while j <= count do begin
+          if ArtO.Key = PriceList.BaseValues[j].Articul.Key then begin
+            PriceList.BaseValues.Delete(PriceList.BaseValues[j]);
+            count := count - 1;
+          end else begin
+            j := j + 1;
           end;
         end;
+      end;
+
+      try
+        BaseValue := PriceList.BaseValues.Add(ArtO);
+        BaseValue.InnerPrice := articulDict[articulDict.Name[i]].Value['INPRICE'];
+        BaseValue.OuterPrice := articulDict[articulDict.Name[i]].Value['OUTPRICE'];
+        BaseValue.BothPrice := articulDict[articulDict.Name[i]].Value['DOUBLEPRICE'];
+        BaseValue.Apply;
+      except
+        visible := false;
+        RaiseException('Артикул "' + ArtO.Marking + '". Ошибка во время установки цен - прайс-лист не может быть создан/обновлён');
       end;
     end;
 
